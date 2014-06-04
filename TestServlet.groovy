053bc7b73994416fbfac2c9489d6f012fc22edd5
leechuck <leechuck@leechuck.de>
2014-06-04 22:28:35 +0100

    added json output, pretty html

--- TestServlet.groovy
+++ TestServlet.groovy
@@ -37,35 +37,83 @@ if (!application.searcher) {
 
 }
 
-def queryString = request.getParameter("query")
+def textquerystring = request.getParameter("query")
 def owlquerystring = request.getParameter("owlquery")
 def ontology = request.getParameter("ontology")?:""
+def output = request.getParameter("output")?:""
 def type = request.getParameter("type")
+def queryString = null
 
-if (queryString == null && owlquerystring != null) {
+if (owlquerystring != null) {
   def result = jsonslurper.parse(new URL("http://jagannath.pdn.cam.ac.uk/aber-owl/service/?type=$type&ontology=$ontology&query="+URLEncoder.encode(owlquerystring))) ;
   queryString = ""
   def max = 1023 // only 1024 query terms allows in Lucene
   if (result.size()<max) {
     max = -1
   }
-  result[0..max].each { res ->
-    if (res.label) {
-      queryString += "\"${res.label}\" OR "
+  if (result.size()==0) { 
+    queryString = "" 
+  } else {
+    result[0..max].each { res ->
+      if (res.label) {
+	queryString += "\"${res.label}\" OR "
+      }
     }
   }
 }
-queryString = queryString.substring(0,queryString.length()-3)
+if (queryString.length()>3) {
+  queryString = queryString.substring(0,queryString.length()-3)
+}
+if (textquerystring == null) {
+  textquerystring = ""
+}
 
-println """
-<!doctype html>
+if (output==null || output=="text") { // text output
+println """<!doctype html>
 <html lang="us">
 <head>
 	<meta charset="utf-8">
 	<title>Aber-OWL: Pubmed - ontology-based access to biomedical literature</title>
-	<link href="css/smoothness/jquery-ui-1.10.4.custom.css" rel="stylesheet">
-	<script src="js/jquery-1.10.2.js"></script>
-	<script src="js/jquery-ui-1.10.4.custom.js"></script>
+	<link href="../css/smoothness/jquery-ui-1.10.4.custom.css" rel="stylesheet">
+	<script src="../js/jquery-1.10.2.js"></script>
+	<script src="../js/jquery-ui-1.10.4.custom.js"></script>
+
+        <script>
+	    \$( "#radioset" ).buttonset();
+
+        \$(document).ready(function() {
+
+        \$('#search').keyup(function(event) {
+        var search_text = \$('#search').val(); 
+        var rg = new RegExp(search_text,'i');
+        \$('#resultlist .abstract').each(function(){ 
+             if(\$.trim(\$(this).html()).search(rg) == -1) {
+             \$(this).parent().css('display', 'none');
+             \$(this).css('display', 'none');
+             \$(this).next().css('display', 'none');
+        }
+         else {
+             \$(this).parent().css('display', '');
+             \$(this).css('display', '');
+             \$(this).next().css('display', '');
+        }
+       });
+});
+
+         \$.ajax({
+	  url:'../service/',
+	  type:'GET',
+	  data: 'type=listontologies',
+	  dataType: 'json',
+	  success: function( json ) {
+            \$.each(json, function(i, value) {
+              \$('#ontologyselector').append(\$('<option>').text(value).attr('value', value));
+            
+               });
+	    }
+         });
+        });
+</script>
 	<style>
 	body{
 		font: 100% "Trebuchet MS", sans-serif;
@@ -104,16 +152,90 @@ println """
 		text-align: center;
 		margin: 0px;
 	}
+	.list {
+	  font-family:sans-serif;
+	  margin:0;
+	  padding:20px 0 0;
+	}
+	.list > li {
+	  display:block;
+	  background-color: #eee;
+	  padding:10px;
+	  box-shadow: inset 0 1px 0 #fff;
+	}
+	.list > h3 {
+	  font-size: 16px;
+	  margin:0 0 0.3rem;
+	  font-weight: normal;
+	  font-weight:bold;
+	}
+	input {
+	  border:solid 1px #ccc;
+	  border-radius: 5px;
+	  padding:7px 14px;
+	  margin-bottom:10px
+	}
+	input:focus {
+	  outline:none;
+	  border-color:#aaa;
+	}
+        #search {
+          float:right;
+          overflow: hidden;
+          position: relative;
+        }
+
 	</style>
 </head>
 <body>
 	<h1 class="title" title="Ontology-based access to biomedical literature">Aber-OWL: Pubmed</h1>
 
+<div id='searchbar'>
+  <form style="margin-top: 1em;" >
+    <div>
+      <center>
+"""
+
+print '<input type="radio" id="radio1" name="type" value="superclass"'
+if (type=="superclass") print ' checked="checked"'
+println '><label for="radio1">Superclasses</label>'
+print '<input type="radio" id="radio1" name="type" value="equivalent"'
+if (type=="equivalent") print ' checked="checked"'
+println '><label for="radio2">Equivalent classes</label>'
+print '<input type="radio" id="radio1" name="type" value="subclass"'
+if (type=="subclass") print ' checked="checked"'
+println '><label for="radio1">Subclasses</label>'
+
+
+println """
+      </center>
+      <center>
+	<select id="ontologyselector" name="ontologyselector">
+	  <option selected="selected" value=""></option>
+	</select>
+
+      </center>
+    </div>
+
+    <center> <input size=100% title=\"OWL query\" type='text' name='owlquery' value=\"$owlquerystring\" />
+<br><br>
+    <center>
+      <input type=submit id="button" value="Submit">
+</input>
+
+</center>
+
+</form>
+</div>
+
   <br/><br/>
 
 <p>
-  <div id="results">
-   <ul>
+  <div id="resultlist" class="resultlist">
+   <ul class="list">
+    <input name="" type="text"  id="search" autocomplete="off" class="search-box-bg" />
+<br><br>
+
 """
 
 
@@ -149,11 +271,11 @@ if (queryString) {
     def pmid = hitDoc.get("pmid")
     def title = hitDoc.get("title")
     if (pmid!=null) {
-      println "<li><h3><a href=\"http://www.ncbi.nlm.nih.gov/pubmed/$pmid\">$title</a></h3>"
+      println "<li><h3 class=\"title\"><a href=\"http://www.ncbi.nlm.nih.gov/pubmed/$pmid\">$title</a></h3>"
       if (pmcid!=null) {
 	println "<p><a href=\"http://www.ncbi.nlm.nih.gov/pmc/articles/PMC$pmcid\">Full text available.</a></p>"
       }
-      println "<p>$frag</p></li>"
+      println "<p class=\"abstract\">$frag</p></li>"
     }
   }
 }
@@ -164,6 +286,22 @@ println """
 
 </p>
 
-</body>
+  </body>
 </html>
 """
+} else if (output == "json") {
+  List l = []
+  def parser = application.parser
+  def searcher = application.searcher
+  def analyzer = application.analyzer
+  if (queryString) {
+    Query query = parser.parse("abstract:($queryString) OR text:($queryString) OR title:($queryString)")
+    ScoreDoc[] hits = searcher.search(query, null, 1000, Sort.RELEVANCE, true, true).scoreDocs
+    hits.each { doc ->
+      Document hitDoc = searcher.doc(doc.doc)
+      l << [ pmcid: hitDoc.get("pmcid"), pmid:hitDoc.get("pmid"), title:hitDoc.get("title") ]
+    }
+  }
+  def builder = new JsonBuilder(l)
+  print builder.toString()
+}
